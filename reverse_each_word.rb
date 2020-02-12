fb1261b39c71445fa614375ef1103f0fddae745c
Diana Ponce <dianap2341@gmail.com>
2020-02-12 20:54:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
  sentences=sentence.split(" ")
- new=[]
-new = sentences.collect do |s|
-  s.reverse
+new=[]
+new = sentences.collect{ |s|s.reverse}
+  
   
   end
   
