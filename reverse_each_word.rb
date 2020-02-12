5bf9d99b7f94f1b79391d306be94d2bb5e071d79
Diana Ponce <dianap2341@gmail.com>
2020-02-12 21:04:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,11 +4,11 @@
 #new = sentences.collect{ |s|s.reverse}
 #new.join(" ")
   def reverse_each_word(string)
-  new = string.split(" ").collect do |str|
-   str.reverse!
-  
-  end
-  new.join(" ")
+new = string.split(" ").collect do |str|
+str.reverse!
+
+end
+new.join(" ")
 end
 
   
