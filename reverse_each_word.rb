8d38cbb2bf4d4fe5c73c65f3586b08cac6f3e7a5
Diana Ponce <dianap2341@gmail.com>
2020-02-12 20:52:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   new=[]
   sentences=sentence.split(" ")
-sentences.collect do |s|
-  new=s.reverse
+new=sentences.collect do |s|
+  s.reverse
   new.join(" ")
   end
   new
